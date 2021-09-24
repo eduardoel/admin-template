16391e5ab32fbcbdc51714fa7b6a42b749718df4
Eduardo Elly <eduardo_elly@hotmail.com>
2021-09-24 20:02:11 -0300

    Initial Construction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Admin
-      </h1>
-    </div>
+    <Layout title="Home page" subtitle="Building an Admin Page">
+      <h3>Content!!!</h3>
+    </Layout>
   )
 }
