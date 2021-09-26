0915743c6dc71545cb6df817e57793c226edc0c9
Eduardo Elly <eduardo_elly@hotmail.com>
2021-09-26 18:03:41 -0300

    API Context - theme 1.0

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -1,9 +1,13 @@
 import Layout from "../components/templates/Layout";
+import useAppData from "../data/hook/useAppData";
 
 export default function Notifications() {
+
+  const context = useAppData()
+
   return (
     <Layout title="Notifications" subtitle="Here you can see your notifications">
-      <h3>Content!!!</h3>
+      <h3>{context.name}</h3>
     </Layout>
   )
 }
