bf6c6ca1b41fe65d21c5670c0576ea636b27931b
Eduardo Elly <eduardo_elly@hotmail.com>
2021-09-27 16:37:59 -0300

    Authentication page #1.5

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -9,13 +9,17 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
     return (
-        <div className={`flex flex-col`}>
+        <div className={`flex flex-col mt-4`}>
             <label>{props.labelAuthInput}</label>
             <input 
                 type={props.typeAuthInputProps ?? 'text'}
                 value={props.valueAuthInput}
                 onChange={e => props.valueChengeAuthInput?.(e.target.value)}
                 required={props.requiredAuthInput}
+                className={`
+                    px-4 py-3 rounded-lg bg-gray-200 mt-2
+                    border focus:border-blue-500 focus:bg-white
+                    focus:outline-none`}
             />
         </div>
     )
--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -7,23 +7,55 @@ export default function Authentication() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    return (
-        <div>
-            <AuthInput
-                labelAuthInput="Email"
-                valueAuthInput={email}
-                typeAuthInputProps="email"
-                valueChengeAuthInput={setEmail}
-                requiredAuthInput
-            />
+    function submit() {
+        if(mode === 'signIn') {
+            console.log('login')
+        } else {
+            console.log('register')
+        }
+    }
 
-            <AuthInput
-                labelAuthInput="Password"
-                valueAuthInput={password}
-                typeAuthInputProps="password"
-                valueChengeAuthInput={setPassword}
-                requiredAuthInput
-            />
+    return (
+        <div className={`flex flex-col h-screen items-center justify-center`}>
+            <div className={`w-1/2`}>
+
+                <h1 className={`
+                    text-xl font-bold mb-5
+                `}>
+                    {mode === 'signIn' ? 'Login with your Account' : 'Register on the Platform'}
+                </h1>
+                <AuthInput
+                    labelAuthInput="Email"
+                    valueAuthInput={email}
+                    typeAuthInputProps="email"
+                    valueChengeAuthInput={setEmail}
+                    requiredAuthInput
+                />
+
+                <AuthInput
+                    labelAuthInput="Password"
+                    valueAuthInput={password}
+                    typeAuthInputProps="password"
+                    valueChengeAuthInput={setPassword}
+                    requiredAuthInput
+                />
+
+                <button onClick={submit} className={`
+                    w-full bg-indigo-500 hover:bg-indigo-400
+                    text-white rounded-lg px-4 py-3 mt-6
+                `}>
+                    {mode === 'signIn' ? 'SignIn' : 'SignUp'}
+                </button>
+
+                <hr className="my-6 border-gray-400 w-full" />
+
+                <button onClick={submit} className={`
+                    w-full bg-red-500 hover:bg-red-400
+                    text-white rounded-lg px-4 py-3
+                `}>
+                    Google
+                </button>
+            </div>
         </div>
     )
 }
