2f77f064b3c07cf004f5594de1b7d7197f6be165
Eduardo Elly <eduardo_elly@hotmail.com>
2021-09-28 00:56:21 -0300

    Authentication page #2.0

--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -1,29 +1,53 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
+import { IconError } from "../components/icons";
 
 export default function Authentication() {
 
+    const [error, setError] = useState(null)
     const [mode, setMode] = useState<'signIn'| 'signUp'>('signIn') // signIn = login | signUp = registration
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    function showError(msg, time = 5) {
+        setError(msg)
+        setTimeout(() => setError(null), time * 1000)
+    }
+
     function submit() {
         if(mode === 'signIn') {
             console.log('login')
+            showError('Login error')
         } else {
             console.log('register')
+            showError('Error to register')
         }
     }
 
     return (
-        <div className={`flex flex-col h-screen items-center justify-center`}>
-            <div className={`w-1/2`}>
-
-                <h1 className={`
-                    text-xl font-bold mb-5
-                `}>
+        <div className="flex h-screen items-center justify-center">
+            <div className="hidden md:block md:w-1/2 lg:w-2/3">
+                <img 
+                    src="https://source.unsplash.com/random"
+                    alt="Image of authentication page"
+                    className="h-screen w-full object-cover" />
+            </div>
+            <div className="m-10 w-full md:w-1/2 lg:w-1/3">
+                <h1 className={`text-3xl font-bold mb-5`}>
                     {mode === 'signIn' ? 'Login with your Account' : 'Register on the Platform'}
                 </h1>
+
+                {error ? (
+                    <div className={`
+                        flex items-center
+                        bg-red-400 text-white py-3 px-5 my-2
+                        border border-red-700 rounded-lg
+                    `}>
+                        {IconError()}
+                        <span className="ml-3">{error}</span>
+                    </div>
+                ): false}
+
                 <AuthInput
                     labelAuthInput="Email"
                     valueAuthInput={email}
@@ -47,7 +71,7 @@ export default function Authentication() {
                     {mode === 'signIn' ? 'SignIn' : 'SignUp'}
                 </button>
 
-                <hr className="my-6 border-gray-400 w-full" />
+                <hr className="my-6 border-gray-300 w-full" />
 
                 <button onClick={submit} className={`
                     w-full bg-red-500 hover:bg-red-400
@@ -55,6 +79,24 @@ export default function Authentication() {
                 `}>
                     Google
                 </button>
+
+                {mode === 'signIn' ? (
+                    <p className="mt-8">
+                        New visitant ?
+                        <a onClick={() => setMode('signUp')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold
+                            cursor-pointer
+                        `}> Create a free account</a>
+                    </p>
+                ): (
+                    <p className="mt-8">
+                        Have an account ?
+                        <a onClick={() => setMode('signIn')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold
+                            cursor-pointer
+                        `}> Login</a>
+                    </p>
+                )}
             </div>
         </div>
     )
